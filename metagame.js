2923ca5b389906a77bb3ec1fbe391e24dc239d80
Seth <sethgoldin27@gmail.com>
2020-09-23 00:11:43 -0400

    experiment with json

--- metagame.js
+++ metagame.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
+const fs = require("fs").promises;
+// const fs = require("fs");
 const skmeans = require("skmeans");
 // globals
 const NUM_CLUSTERS = 20;
@@ -87,122 +88,144 @@ function set(arr) {
         return seen;
     }, {}));
 }
-fs.readFile("decks_json/decks-" + FORMATS[0] + ".json", "utf8", function (err, json) {
-    var decks_json = JSON.parse(json);
-    // console.log(JSON.stringify(decks_json));
-    for (let i of Object.keys(decks_json)) {
-        var deck_of_cards = [];
-        for (let card of decks_json[i]["main"]) {
-            // initialize deck dict, determine card data
-            deck_of_cards.push([card["quantity"], card["name"]]);
-            all_cards.push(card["name"]);
-            if (!IGNORE.some(c => card["name"].includes(c))) {
-                cards_w_ignore.push(card["name"]);
-            }
-            let idx = unique_cards.findIndex(c => c.card_name == card.name);
-            if (idx == -1) {
-                unique_cards.push({
-                    card_name: card["name"],
-                    quantity: card["quantity"]
-                });
-            }
-            else if (!IGNORE.some(c => card.name.includes(c))) {
-                unique_cards[idx].quantity += card.quantity;
-            }
+// var decks_json: JSON = await fs.readFile(
+// 	"decks_json/decks-" + FORMATS[0] + ".json",
+// 	"utf8",
+// 	function (err: string, json: string) {
+// 		JSON.parse(json);
+// 		// console.log(JSON.stringify(decks_json));
+// 	}
+// );
+// var decks_json: JSON = fs.readFileSync(
+// 	"decks_json/decks-" + FORMATS[0] + ".json",
+// 	"utf8"
+// );
+// var decks_json: JSON = await fs.readFile(
+// 	"decks_json/decks-" + FORMATS[0] + ".json",
+// 	"utf8"
+// ); // need
+// function loadJson() {
+// 	return fs.readFile("decks_json/decks-" + FORMATS[0] + ".json", "utf8");
+// }
+var decks_json = fs.readFile("decks_json/decks-" + FORMATS[0] + ".json", "utf8");
+// (async () => {
+// 	var decks_json: JSON = await fs.readFile(
+// 		"decks_json/decks-" + FORMATS[0] + ".json",
+// 		"utf8"
+// 	); // need
+// })();
+for (let i of Object.keys(decks_json)) {
+    var deck_of_cards = [];
+    for (let card of decks_json[i]["main"]) {
+        // initialize deck dict, determine card data
+        deck_of_cards.push([card["quantity"], card["name"]]);
+        all_cards.push(card["name"]);
+        if (!IGNORE.some(c => card["name"].includes(c))) {
+            cards_w_ignore.push(card["name"]);
         }
-        decks.push(deck_of_cards);
-    }
-    var format_json = {
-        archetypes: [],
-        format_top_cards: [],
-        format_versatile_cards: [],
-        total_cards_parsed: all_cards.length,
-        unique_cards_parsed: unique_cards.length
-    };
-    // Determine "deck vectors" - translate mtg decks to a format that can be used for KM++
-    function deck_to_vector(input_deck) {
-        var v = Array(all_cards.length).fill(0);
-        for (const [x, name] of all_cards.entries()) {
-            for (const [idx, card] of input_deck.entries()) {
-                if (card[1] == name) {
-                    v[x] += card[0];
-                }
-            }
+        let idx = unique_cards.findIndex(c => c.card_name == card.name);
+        if (idx == -1) {
+            unique_cards.push({
+                card_name: card["name"],
+                quantity: card["quantity"]
+            });
         }
-        return v;
-    }
-    var deck_vectors = [];
-    for (var deck of decks) {
-        deck_vectors.push(deck_to_vector(deck));
-    }
-    // Determine meta using K-Means++ clustering
-    var kmeans = skmeans(deck_vectors, NUM_CLUSTERS, "kmpp");
-    var deck_zip = zipDeck(decks, kmeans.idxs);
-    // Translate K-Means data to a format that can be parsed
-    var card_counts = [];
-    for (var i in [...Array(NUM_CLUSTERS).keys()]) {
-        card_counts.push([parseInt(i), decksByIdx(parseInt(i), deck_zip).length]);
-    }
-    var total_instances = 0;
-    card_counts.forEach(function (a, b) {
-        total_instances += a[1];
-    });
-    // FOR EACH CLUSTER
-    for (let i in [...Array(NUM_CLUSTERS).keys()]) {
-        // Define cluster - Instead of taking the intersection of all the decks in a cluster, which could lead to archetype staples being excluded due to variance, this method involves taking every deck in the cluster and finding the most common cards (or archetype staples)
-        var card_set = [];
-        for (let deck_item of decksByIdx(parseInt(i), deck_zip)) {
-            card_set.push(set(mostCommonCards(deck_item[0], 40)));
+        else if (!IGNORE.some(c => card.name.includes(c))) {
+            unique_cards[idx].quantity += card.quantity;
         }
-        let card_list = Array.prototype.concat.apply([], card_set);
-        let count_cards = card_list.reduce((a, b) => {
-            a[b] = (a[b] || 0) + 1;
-            return a;
-        }, {});
-        let sorted_cards = Object.keys(count_cards)
-            .map(function (k) {
-            return [k, count_cards[k]];
-        })
-            .sort(function (a, b) {
-            return b[1] - a[1];
-        });
-        var cluster = [];
-        for (var card_item of sorted_cards.slice(0, 20)) {
-            cluster.push(card_item[0]);
-        }
-        // calculate percentage of meta, deck name, best_fit deck
-        var instances = decksByIdx(parseInt(i), deck_zip).length;
-        var deck_archetype = {
-            archetype_name: "Unknown",
-            top_cards: cluster,
-            metagame_percentage: ((instances / total_instances) * 100).toFixed(2) + "%",
-            best_fit_deck: { main: [], sb: [] }
-        };
-        let max_similar = 0;
-        for (const deck_obj of Object.values(decks_json)) {
-            let similar = 0;
-            for (const card of deck_obj["main"]) {
-                if (cluster.includes(card["name"])) {
-                    similar += 1;
-                }
-                if (similar > max_similar) {
-                    max_similar = similar;
-                    deck_archetype.archetype_name = deck_obj["name"];
-                    deck_archetype.best_fit_deck = {
-                        main: deck_obj["main"],
-                        sb: deck_obj["sb"]
-                    };
-                }
+    }
+    decks.push(deck_of_cards);
+}
+var format_json = {
+    archetypes: [],
+    format_top_cards: [],
+    format_versatile_cards: [],
+    total_cards_parsed: all_cards.length,
+    unique_cards_parsed: unique_cards.length
+};
+// Determine "deck vectors" - translate mtg decks to a format that can be used for KM++
+function deck_to_vector(input_deck) {
+    var v = Array(all_cards.length).fill(0);
+    for (const [x, name] of all_cards.entries()) {
+        for (const [idx, card] of input_deck.entries()) {
+            if (card[1] == name) {
+                v[x] += card[0];
             }
         }
-        console.log("\nCluster #" + i + " (" + deck_archetype.archetype_name + ") :");
-        console.log(deck_archetype.top_cards);
-        format_json.archetypes.push(deck_archetype);
     }
-    // Sort JSON
-    format_json["archetypes"].sort((a, b) => parseFloat(b.metagame_percentage.replace("%", "")) -
-        parseFloat(a.metagame_percentage.replace("%", "")));
-    format_json["format_versatile_cards"].sort((a, b) => b.total_instances - a.total_instances);
-    // Write JSON Output
-    fs.writeFile(FORMATS[0] + ".json", JSON.stringify(format_json, null, 4), "utf8", function (err, data) { });
+    return v;
+}
+var deck_vectors = [];
+for (var deck of decks) {
+    deck_vectors.push(deck_to_vector(deck));
+}
+// Determine meta using K-Means++ clustering
+var kmeans = skmeans(deck_vectors, NUM_CLUSTERS, "kmpp");
+var deck_zip = zipDeck(decks, kmeans.idxs);
+// Translate K-Means data to a format that can be parsed
+var card_counts = [];
+for (var i in [...Array(NUM_CLUSTERS).keys()]) {
+    card_counts.push([parseInt(i), decksByIdx(parseInt(i), deck_zip).length]);
+}
+var total_instances = 0;
+card_counts.forEach(function (a, b) {
+    total_instances += a[1];
 });
+// FOR EACH CLUSTER
+for (let i in [...Array(NUM_CLUSTERS).keys()]) {
+    // Define cluster - Instead of taking the intersection of all the decks in a cluster, which could lead to archetype staples being excluded due to variance, this method involves taking every deck in the cluster and finding the most common cards (or archetype staples)
+    var card_set = [];
+    for (let deck_item of decksByIdx(parseInt(i), deck_zip)) {
+        card_set.push(set(mostCommonCards(deck_item[0], 40)));
+    }
+    let card_list = Array.prototype.concat.apply([], card_set);
+    let count_cards = card_list.reduce((a, b) => {
+        a[b] = (a[b] || 0) + 1;
+        return a;
+    }, {});
+    let sorted_cards = Object.keys(count_cards)
+        .map(function (k) {
+        return [k, count_cards[k]];
+    })
+        .sort(function (a, b) {
+        return b[1] - a[1];
+    });
+    var cluster = [];
+    for (var card_item of sorted_cards.slice(0, 20)) {
+        cluster.push(card_item[0]);
+    }
+    // calculate percentage of meta, deck name, best_fit deck
+    var instances = decksByIdx(parseInt(i), deck_zip).length;
+    var deck_archetype = {
+        archetype_name: "Unknown",
+        top_cards: cluster,
+        metagame_percentage: ((instances / total_instances) * 100).toFixed(2) + "%",
+        best_fit_deck: { main: [], sb: [] }
+    };
+    let max_similar = 0;
+    for (const deck_obj of Object.values(decks_json)) {
+        let similar = 0;
+        for (const card of deck_obj["main"]) {
+            if (cluster.includes(card["name"])) {
+                similar += 1;
+            }
+            if (similar > max_similar) {
+                max_similar = similar;
+                deck_archetype.archetype_name = deck_obj["name"];
+                deck_archetype.best_fit_deck = {
+                    main: deck_obj["main"],
+                    sb: deck_obj["sb"]
+                };
+            }
+        }
+    }
+    console.log("\nCluster #" + i + " (" + deck_archetype.archetype_name + ") :");
+    console.log(deck_archetype.top_cards);
+    format_json.archetypes.push(deck_archetype);
+}
+// Sort JSON
+format_json["archetypes"].sort((a, b) => parseFloat(b.metagame_percentage.replace("%", "")) -
+    parseFloat(a.metagame_percentage.replace("%", "")));
+format_json["format_versatile_cards"].sort((a, b) => b.total_instances - a.total_instances);
+// Write JSON Output
+fs.writeFile(FORMATS[0] + ".json", JSON.stringify(format_json, null, 4), "utf8", function (err, data) { });
--- metagame.ts
+++ metagame.ts
@@ -1,4 +1,5 @@
-var fs = require("fs");
+const fs = require("fs").promises;
+// const fs = require("fs");
 const skmeans = require("skmeans");
 
 export interface Kmeans {
@@ -154,142 +155,167 @@ function set(arr): Array<string> {
 	);
 }
 
-fs.readFile("decks_json/decks-" + FORMATS[0] + ".json", "utf8", function (
-	err: string,
-	json: string
-) {
-	var decks_json: JSON = JSON.parse(json);
-    // console.log(JSON.stringify(decks_json));
-    
-	for (let i of Object.keys(decks_json)) {
-		var deck_of_cards: Deck = [];
-		for (let card of decks_json[i]["main"]) {
-			// initialize deck dict, determine card data
-			deck_of_cards.push([card["quantity"], card["name"]]);
-			all_cards.push(card["name"]);
-			if (!IGNORE.some(c => card["name"].includes(c))) {
-				cards_w_ignore.push(card["name"]);
-			}
-			let idx = unique_cards.findIndex(c => c.card_name == card.name);
-			if (idx == -1) {
-				unique_cards.push({
-					card_name: card["name"],
-					quantity: card["quantity"]
-				});
-			} else if (!IGNORE.some(c => card.name.includes(c))) {
-				unique_cards[idx].quantity += card.quantity;
-			}
-		}
-		decks.push(deck_of_cards);
-	}
+// var decks_json: JSON = await fs.readFile(
+// 	"decks_json/decks-" + FORMATS[0] + ".json",
+// 	"utf8",
+// 	function (err: string, json: string) {
+// 		JSON.parse(json);
+// 		// console.log(JSON.stringify(decks_json));
+// 	}
+// );
 
-	var format_json: FormatJson = {
-		archetypes: [],
-		format_top_cards: [],
-		format_versatile_cards: [],
-		total_cards_parsed: all_cards.length,
-		unique_cards_parsed: unique_cards.length
-	};
+// var decks_json: JSON = fs.readFileSync(
+// 	"decks_json/decks-" + FORMATS[0] + ".json",
+// 	"utf8"
+// );
 
-	// Determine "deck vectors" - translate mtg decks to a format that can be used for KM++
-	function deck_to_vector(input_deck: Deck): Array<number> {
-		var v: Array<number> = Array(all_cards.length).fill(0);
-		for (const [x, name] of all_cards.entries()) {
-			for (const [idx, card] of input_deck.entries()) {
-				if (card[1] == name) {
-					v[x] += card[0];
-				}
-			}
-		}
-		return v;
-	}
-	var deck_vectors: Array<Array<number>> = [];
-	for (var deck of decks) {
-		deck_vectors.push(deck_to_vector(deck));
-	}
-	// Determine meta using K-Means++ clustering
-	var kmeans: Kmeans = skmeans(deck_vectors, NUM_CLUSTERS, "kmpp");
-	var deck_zip: Array<[Deck, number]> = zipDeck(decks, kmeans.idxs);
-	// Translate K-Means data to a format that can be parsed
-	var card_counts: Array<[number, number]> = [];
-	for (var i in [...Array(NUM_CLUSTERS).keys()]) {
-		card_counts.push([parseInt(i), decksByIdx(parseInt(i), deck_zip).length]);
-	}
-	var total_instances: number = 0;
-	card_counts.forEach(function (a, b) {
-		total_instances += a[1];
-	});
+// var decks_json: JSON = await fs.readFile(
+// 	"decks_json/decks-" + FORMATS[0] + ".json",
+// 	"utf8"
+// ); // need
 
-	// FOR EACH CLUSTER
-	for (let i in [...Array(NUM_CLUSTERS).keys()]) {
-		// Define cluster - Instead of taking the intersection of all the decks in a cluster, which could lead to archetype staples being excluded due to variance, this method involves taking every deck in the cluster and finding the most common cards (or archetype staples)
-		var card_set: Array<Array<string>> = [];
-		for (let deck_item of decksByIdx(parseInt(i), deck_zip)) {
-			card_set.push(set(mostCommonCards(deck_item[0], 40)));
+// function loadJson() {
+// 	return fs.readFile("decks_json/decks-" + FORMATS[0] + ".json", "utf8");
+// }
+
+var decks_json: JSON = fs.readFile(
+	"decks_json/decks-" + FORMATS[0] + ".json",
+	"utf8"
+);
+
+// (async () => {
+// 	var decks_json: JSON = await fs.readFile(
+// 		"decks_json/decks-" + FORMATS[0] + ".json",
+// 		"utf8"
+// 	); // need
+// })();
+
+for (let i of Object.keys(decks_json)) {
+	var deck_of_cards: Deck = [];
+	for (let card of decks_json[i]["main"]) {
+		// initialize deck dict, determine card data
+		deck_of_cards.push([card["quantity"], card["name"]]);
+		all_cards.push(card["name"]);
+		if (!IGNORE.some(c => card["name"].includes(c))) {
+			cards_w_ignore.push(card["name"]);
 		}
-		let card_list: Array<string> = Array.prototype.concat.apply([], card_set);
-		let count_cards = card_list.reduce((a, b) => {
-			a[b] = (a[b] || 0) + 1;
-			return a;
-		}, {});
-		let sorted_cards = Object.keys(count_cards)
-			.map(function (k) {
-				return [k, count_cards[k]];
-			})
-			.sort(function (a, b) {
-				return b[1] - a[1];
+		let idx = unique_cards.findIndex(c => c.card_name == card.name);
+		if (idx == -1) {
+			unique_cards.push({
+				card_name: card["name"],
+				quantity: card["quantity"]
 			});
-		var cluster: Array<String> = [];
-		for (var card_item of sorted_cards.slice(0, 20)) {
-			cluster.push(card_item[0]);
+		} else if (!IGNORE.some(c => card.name.includes(c))) {
+			unique_cards[idx].quantity += card.quantity;
 		}
-		// calculate percentage of meta, deck name, best_fit deck
-		var instances: number = decksByIdx(parseInt(i), deck_zip).length;
-		var deck_archetype: Archetype = {
-			archetype_name: "Unknown",
-			top_cards: cluster,
-			metagame_percentage:
-				((instances / total_instances) * 100).toFixed(2) + "%",
-			best_fit_deck: { main: [], sb: [] }
-		};
-		let max_similar: number = 0;
-		for (const deck_obj of Object.values(decks_json)) {
-			let similar: number = 0;
-			for (const card of deck_obj["main"]) {
-				if (cluster.includes(card["name"])) {
-					similar += 1;
-				}
-				if (similar > max_similar) {
-					max_similar = similar;
-					deck_archetype.archetype_name = deck_obj["name"];
-					deck_archetype.best_fit_deck = {
-						main: deck_obj["main"],
-						sb: deck_obj["sb"]
-					};
-				}
+	}
+	decks.push(deck_of_cards);
+}
+
+var format_json: FormatJson = {
+	archetypes: [],
+	format_top_cards: [],
+	format_versatile_cards: [],
+	total_cards_parsed: all_cards.length,
+	unique_cards_parsed: unique_cards.length
+};
+
+// Determine "deck vectors" - translate mtg decks to a format that can be used for KM++
+function deck_to_vector(input_deck: Deck): Array<number> {
+	var v: Array<number> = Array(all_cards.length).fill(0);
+	for (const [x, name] of all_cards.entries()) {
+		for (const [idx, card] of input_deck.entries()) {
+			if (card[1] == name) {
+				v[x] += card[0];
 			}
 		}
-		console.log(
-			"\nCluster #" + i + " (" + deck_archetype.archetype_name + ") :"
-		);
-		console.log(deck_archetype.top_cards);
-		format_json.archetypes.push(deck_archetype);
 	}
+	return v;
+}
+var deck_vectors: Array<Array<number>> = [];
+for (var deck of decks) {
+	deck_vectors.push(deck_to_vector(deck));
+}
+// Determine meta using K-Means++ clustering
+var kmeans: Kmeans = skmeans(deck_vectors, NUM_CLUSTERS, "kmpp");
+var deck_zip: Array<[Deck, number]> = zipDeck(decks, kmeans.idxs);
+// Translate K-Means data to a format that can be parsed
+var card_counts: Array<[number, number]> = [];
+for (var i in [...Array(NUM_CLUSTERS).keys()]) {
+	card_counts.push([parseInt(i), decksByIdx(parseInt(i), deck_zip).length]);
+}
+var total_instances: number = 0;
+card_counts.forEach(function (a, b) {
+	total_instances += a[1];
+});
 
-	// Sort JSON
-	format_json["archetypes"].sort(
-		(a, b) =>
-			parseFloat(b.metagame_percentage.replace("%", "")) -
-			parseFloat(a.metagame_percentage.replace("%", ""))
-	);
-	format_json["format_versatile_cards"].sort(
-		(a, b) => b.total_instances - a.total_instances
-	);
+// FOR EACH CLUSTER
+for (let i in [...Array(NUM_CLUSTERS).keys()]) {
+	// Define cluster - Instead of taking the intersection of all the decks in a cluster, which could lead to archetype staples being excluded due to variance, this method involves taking every deck in the cluster and finding the most common cards (or archetype staples)
+	var card_set: Array<Array<string>> = [];
+	for (let deck_item of decksByIdx(parseInt(i), deck_zip)) {
+		card_set.push(set(mostCommonCards(deck_item[0], 40)));
+	}
+	let card_list: Array<string> = Array.prototype.concat.apply([], card_set);
+	let count_cards = card_list.reduce((a, b) => {
+		a[b] = (a[b] || 0) + 1;
+		return a;
+	}, {});
+	let sorted_cards = Object.keys(count_cards)
+		.map(function (k) {
+			return [k, count_cards[k]];
+		})
+		.sort(function (a, b) {
+			return b[1] - a[1];
+		});
+	var cluster: Array<String> = [];
+	for (var card_item of sorted_cards.slice(0, 20)) {
+		cluster.push(card_item[0]);
+	}
+	// calculate percentage of meta, deck name, best_fit deck
+	var instances: number = decksByIdx(parseInt(i), deck_zip).length;
+	var deck_archetype: Archetype = {
+		archetype_name: "Unknown",
+		top_cards: cluster,
+		metagame_percentage: ((instances / total_instances) * 100).toFixed(2) + "%",
+		best_fit_deck: { main: [], sb: [] }
+	};
+	let max_similar: number = 0;
+	for (const deck_obj of Object.values(decks_json)) {
+		let similar: number = 0;
+		for (const card of deck_obj["main"]) {
+			if (cluster.includes(card["name"])) {
+				similar += 1;
+			}
+			if (similar > max_similar) {
+				max_similar = similar;
+				deck_archetype.archetype_name = deck_obj["name"];
+				deck_archetype.best_fit_deck = {
+					main: deck_obj["main"],
+					sb: deck_obj["sb"]
+				};
+			}
+		}
+	}
+	console.log("\nCluster #" + i + " (" + deck_archetype.archetype_name + ") :");
+	console.log(deck_archetype.top_cards);
+	format_json.archetypes.push(deck_archetype);
+}
 
-	// Write JSON Output
-	fs.writeFile(
-		FORMATS[0] + ".json",
-		JSON.stringify(format_json, null, 4),
-		"utf8",
-		function (err: String, data: String) {}
-	);
+// Sort JSON
+format_json["archetypes"].sort(
+	(a, b) =>
+		parseFloat(b.metagame_percentage.replace("%", "")) -
+		parseFloat(a.metagame_percentage.replace("%", ""))
+);
+format_json["format_versatile_cards"].sort(
+	(a, b) => b.total_instances - a.total_instances
+);
+
+// Write JSON Output
+fs.writeFile(
+	FORMATS[0] + ".json",
+	JSON.stringify(format_json, null, 4),
+	"utf8",
+	function (err: String, data: String) {}
+);
